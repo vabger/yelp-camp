853e6aa03b1d1239c43d608ce57987c1b2c254a7
Vaibhav Gera <vgera2001@gmail.com>
2021-12-13 00:50:06 +0530

    added loading bars for LOG_IN_INITIATE action

---
+++ client/src/components/loading/LoadingBars.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './LoadingBars.css'
+
+function LoadingBars() {
+    return (
+        <div className="ld-bars"><div></div><div></div><div></div></div>
+    )
+}
+
+export default LoadingBars
