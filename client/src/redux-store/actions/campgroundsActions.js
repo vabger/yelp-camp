5bb29f5dec7a1e614ef647c67bd5c02ae1cb7e32
Vaibhav Gera <vgera2001@gmail.com>
2021-12-15 01:31:39 +0530

    added the campgroundDetails state in redux store with getCampgroundById action

--- client/src/redux-store/actions/campgroundsActions.js
+++ client/src/redux-store/actions/campgroundsActions.js
@@ -2,6 +2,9 @@ import {
   CAMPGROUNDS_FAILURE,
   CAMPGROUNDS_FETCH,
   CAMPGROUNDS_SUCCESS,
+  CAMPGROUND_BY_ID_FAILURE,
+  CAMPGROUND_BY_ID_FETCH,
+  CAMPGROUND_BY_ID_SUCCESS,
 } from "../action-types/campgroundsTypes";
 
 import { loadingStart, loadingStop } from "./uiActions";
@@ -34,3 +37,19 @@ export const fetchCampgrounds = () => {
     })
   }
 };
+
+export const getCampgroundById = (id) => {
+  return (dispatch, getState) => {
+    dispatch(loadingStart(CAMPGROUND_BY_ID_FETCH))
+    axios.get(`http://localhost:5000/campgrounds/${id}`)
+      .then((res) => {
+        dispatch({ type: CAMPGROUND_BY_ID_SUCCESS, payload: res.data.campground })
+      })
+      .catch((err) => {
+        dispatch({ type: CAMPGROUND_BY_ID_FAILURE, payload: err.response.data.error })
+      })
+      .finally(() => {
+        dispatch(loadingStop(CAMPGROUND_BY_ID_FETCH))
+      })
+  }
+}
