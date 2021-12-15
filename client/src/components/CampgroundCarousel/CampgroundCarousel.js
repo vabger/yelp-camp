63566e0ab492b79051e5b671852ab4a425e69e65
Vaibhav Gera <vgera2001@gmail.com>
2021-12-16 03:35:59 +0530

    Improved campground carousel

--- client/src/components/CampgroundCarousel/CampgroundCarousel.js
+++ client/src/components/CampgroundCarousel/CampgroundCarousel.js
@@ -6,23 +6,32 @@ import next from "../../icons/right-arrow.png"
 import previous from "../../icons/left-arrow.png"
 
 function CampgroundCarousel({ images }) {
+
+    const mapButton = (image, index) => {
+        if (index === 0)
+            return <button type="button" data-bs-target="#CampgroundCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 0" key={index} />
+
+        return <button type="button" data-bs-target="#CampgroundCarousel" data-bs-slide-to={index} aria-label={`slide ${index}`} key={index} />
+    }
+
+    const mapImg = (image, index) => {
+        if (index === 0)
+            return (<div className="carousel-item active" data-bs-interval="10000" key={index}>
+                <img src={image.url} alt="campground" />
+            </div>)
+        return (<div className="carousel-item" data-bs-interval="2000" key={index}>
+            <img src={image.url} alt="campground" />
+        </div>)
+
+    }
+
     return (
         <div id="CampgroundCarousel" className="carousel carousel-dark slide" data-bs-ride="carousel">
             <div className="carousel-indicators">
-                <button type="button" data-bs-target="#CampgroundCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#CampgroundCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#CampgroundCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                {images.map(mapButton)}
             </div>
             <div className="carousel-inner">
-                <div className="carousel-item active" data-bs-interval="10000">
-                    <img src={images[0].url} alt="first" />
-                </div>
-                <div className="carousel-item" data-bs-interval="2000">
-                    <img src={images[1].url} alt="second" />
-                </div>
-                <div className="carousel-item">
-                    <img src={images[2].url} alt="third" />
-                </div>
+                {images.map(mapImg)}
             </div>
             <div className="carousel-control-prev" type="button" data-bs-target="#CampgroundCarousel" data-bs-slide="prev">
                 <img src={previous} alt='previous' />
