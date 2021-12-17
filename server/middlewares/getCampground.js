342e3489a647e00d769651893a57333e7f517296
Vaibhav Gera <vgera2001@gmail.com>
2021-12-17 19:20:37 +0530

    added a 404 error whenever an invalid campground id is requested

--- server/middlewares/getCampground.js
+++ server/middlewares/getCampground.js
@@ -1,13 +1,21 @@
 const CampGround = require("../models/CampGround");
 const ExpressError = require("../utils/ExpressError");
 const catchAsync = require("../utils/catchAsync");
+const mongoose = require("mongoose")
+
 module.exports = catchAsync(async (req, res, next) => {
-  const campground = await CampGround.findById(req.params.id).populate({
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new ExpressError(404, "Can't find what you are looking for!")
+  }
+
+  const campground = await CampGround.findById(id).populate({
     path: "reviews",
     populate: {
       path: "user",
     },
   });
+
   if (!campground) {
     throw new ExpressError(404, "Campground not found!");
   }
